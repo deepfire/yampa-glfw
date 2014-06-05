3c968721faf4b041a3cee15af33a3fc4095d3dc4
Samium Gromoff <_deepfire@feelingofgreen.ru>
2014-06-05 20:58:55 +0400

    example / time fluctuation:  debug infrastructure

--- example.hs
+++ example.hs
@@ -6,11 +6,16 @@ import Data.VectorSpace
 import "GLFW-b" Graphics.UI.GLFW
 import Graphics.Rendering.OpenGL
 import FRP.Yampa.GLFW.Adapter
-import FRP.Yampa (SF, integral, delay, initially, edge, accumHoldBy)
+import FRP.Yampa (SF, integral, delay, initially, edge, accumHoldBy, time)
 import FRP.Yampa.Event
+
+import GHC.Float
+
+import Text.Printf
+
 import Unsafe.Coerce
-import Debug.Trace
 import System.IO.Unsafe
+import Data.IORef
 
 checkError :: String -> IO ()
 checkError functionName = get errors >>= mapM_ reportError
@@ -80,15 +85,35 @@ reshape sz@(Size w h) = do
     loadIdentity
 
     translate (Vector3 0 0 (-4 :: GLfloat))
+
+pr :: String -> b -> b
+pr x exp = seq (unsafePerformIO $ putStrLn x) exp
+
+oldRef = unsafePerformIO $ newIORef (0 :: Int, 0.0, (0, 0))
+
+v2len :: (Float, Float) -> Float
+v2len (x, y) = sqrt(x*x + y*y)
+
+watcher :: Double -> (Float, Float) -> IO ()
+watcher t pos = do
+  (oldi, oldt, oldp) <- readIORef oldRef
+  writeIORef oldRef (oldi + 1, t, pos)
+  let dt = t - oldt
+      dp = v2len $ pos ^-^ oldp
+  printf "; %d t=%3.3f dt=%3.3f dp=%3.3f es=%3.3f\n" oldi t dt dp (dp / (double2Float dt))
+  return ()
+
 positionSim :: SF (Event UI) (Float, Float)
 positionSim = proc ev -> do
                d <- moveDirection -< ev
                (Vector2 tx ty) <- simpleMousePosition -< ev
+               t <- time -< ev
                rec
                    let mpos = (tx, ty)
                        dpos = mpos ^-^ pos
                        speed = normalized dpos ^* 0.5 ^* d
-                   pos <- integral <<< delay 0.2 zeroV -< speed
-               returnA -< pos
+                   pos <- integral <<< delay 0.1 zeroV -< speed
+               returnA -< -- seq (unsafePerformIO $ watcher t pos)
+                                 pos
 moveDirection :: SF (Event UI) Float
 moveDirection = mouseButtonPressed MouseButton0 >>> edge >>> accumHoldBy (const . negate) 1
